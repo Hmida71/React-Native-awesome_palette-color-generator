d3e1773e28316fff9c46e005bc7b2a141f0535b5
Hmida <69757558+Hmida71@users.noreply.github.com>
2021-10-02 09:17:23 +0100

    Create About.js

---
+++ src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const About = () => {
+  return (
+    <View style={styles.center}>
+      <Text>This is the about screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+});
+
+export default About;
